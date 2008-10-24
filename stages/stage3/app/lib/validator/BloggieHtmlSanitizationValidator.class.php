27ddd19c832b3615ba547e8459efc730ea817411
Mike Seth <me@mikeseth.com>
2008-10-24 19:01:39 +0000

    added big fat warnings about the evil validator

--- stages/stage3/app/lib/validator/BloggieHtmlSanitizationValidator.class.php
+++ stages/stage3/app/lib/validator/BloggieHtmlSanitizationValidator.class.php
@@ -3,7 +3,12 @@
   /**
    * A very primitive validator that sanitizes given HTML by running
    * htmlentities() over it. 
+
+   * DO NOT USE THIS VALIDATOR IN YOUR APPLICATIONS. IT EXISTS FOR
+   * DEMONSTRATIONAL PURPOSE ONLY. IT IS WRONG AND HARMFUL TO STORE
+   * HTML ENTITIES IN THE DATABASE.
    */
+
 class BloggieHtmlSanitizationValidator extends AgaviValidator
 {
   public function validate()
