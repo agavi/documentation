4295b59791ec6604aa7c5b3a654ea932033bfd67
Mike Seth <me@mikeseth.com>
2008-10-26 22:39:58 +0000

    elaborations and cleanup, closes #3 and closes #4

--- stages/stage3/app/modules/Admin/actions/LoginAction.class.php
+++ stages/stage3/app/modules/Admin/actions/LoginAction.class.php
@@ -6,23 +6,30 @@ class Admin_LoginAction extends BlogAdminBaseAction
   {
     $um = $this->context->getModel('Users', 'Admin');
 
+    // Attempt to authenticate
     if ($urec = $um->authenticateAccount($rd->getParameter('username'), $rd->getParameter('password')))
     {
+      // We found the user record, let's start the session
       $us = $this->context->getUser();
       $us->setAuthenticated(true);
       $us->clearCredentials();
 
-      // This is used when saving blog posts
+      // This is the identity of the user. It persists in the session.
       $us->setAttribute('author_id', $urec['id']);
+
+      // Now that we're authenticated, perform a redirect to admin
+      // home page
 
       return 'ForwardToMain';
     }
     
+    // No such user in the database, complain
     return 'Error';
   }
 
   public function getDefaultViewName()
   {
+    // Show the login form
     return 'Success';
   }
 }
--- stages/stage3/app/modules/Public/templates/decorator.php
+++ stages/stage3/app/modules/Public/templates/decorator.php
@@ -2,43 +2,55 @@
 "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html>
 <head>
-<meta http-equiv="content-type" content="text/html; charset=iso-8859-1"/>
-<meta name="description" content="description"/>
-<meta name="keywords" content="keywords"/> 
-<meta name="author" content="author"/> 
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+<meta name="description" content="Agavi Demo Blog Application"/>
+<meta name="keywords" content="agavi, demo, win, god"/> 
+<meta name="author" content="Agavi Development Team"/> 
 <link rel="stylesheet" type="text/css" href="/public/default.css" media="screen"/>
 <base href="<?php print $ro->getBaseHref(); ?>"/>
 <title><?php print $template['title'];?></title>
 </head>
 <body>
 <div class="container">
-   <div class="title" style="	background: url(/agavi.png) no-repeat; padding-left: 64px; "/>
-   <h1 id="title"><a href="<?php print $ro->gen('home'); ?>">Agavi Demo Blog</a></h1>
-   <?php if ($template['title']): ?><h2><?php print $template['title'];?></h2><?php endif;?>
-	</div>
+<div class="title" style="background: url(/agavi.png) no-repeat; padding-left: 64px; "/>
+  <h1 id="title"><a href="<?php print $ro->gen('home'); ?>">Agavi Demo Blog</a></h1>
+  <?php if ($template['title']): ?><h2><?php print $template['title'];?></h2><?php endif;?>
+</div>
 
-	<div class="navigation">
-                <?php print $slots['navigation']; ?>
-		<div class="clearer"><span></span></div>
-	</div>
+<div class="navigation">
+  
+  <?php
 
-	<div class="holder_top"></div>
-	<div class="holder">
-   <?php
+  /* Print a slot. The layout configuration specifies which Action
+  goes into this slot - the default layout loads ShowNavigation. Admin
+  module has its own ShowNavigation, and the base View class for the
+  Admin module is instructed to load the admin layout by default, in
+  which the Public ShowNavigation is overridden with Admin
+  ShowNavigation */
 
-   /* Print whatever is in the layer above this - typically an Action's output */
-   print $inner; 
-   
-   ?>
-	</div>
-	<div class="holder_bottom"></div>
+  print $slots['navigation'];
 
-	<div class="holder_top"></div>
-	<div class="holder">
-		<div class="footer">&#169; 2008 <a href="index.html">bloggie.dev</a>. Valid <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> &amp; <a href="http://validator.w3.org/check?uri=referer">XHTML</a>. Template design by <a href="http://arcsin.se">Arcsin</a>
-		</div>
-	</div>
-	<div class="holder_bottom"></div>
+  ?>
+<div class="clearer"><span></span></div>
+</div>
+
+<div class="holder_top"></div>
+<div class="holder">
+  <?php
+
+  /* Print whatever is in the layer above this - normally an Action's output */
+  print $inner; 
+  
+  ?>
+</div>
+<div class="holder_bottom"></div>
+
+<div class="holder_top"></div>
+<div class="holder">
+<div class="footer">&#169; 2008 <a href="index.html">bloggie.dev</a>. Valid <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> &amp; <a href="http://validator.w3.org/check?uri=referer">XHTML</a>. Template design by <a href="http://arcsin.se">Arcsin</a>
+</div>
+</div>
+<div class="holder_bottom"></div>
 </div>
 </body>
 </html>
