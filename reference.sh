a1287ce1cf6ed0b4eb891dae279c0eccbd21037c
Mike Seth <me@mikeseth.com>
2008-10-27 11:28:39 +0000

    initial version of the reference guide

---
+++ reference.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+PROJ_DIR=`pwd`
+cd $DITA_HOME
+
+# Get the absolute path of DITAOT's home directory
+DITA_DIR="`pwd`"
+export ANT_OPTS="-Xmx512m $ANT_OPTS"
+export ANT_HOME="$DITA_DIR"/tools/ant
+export PATH="$DITA_DIR"/tools/ant/bin:"$PATH"
+
+NEW_CLASSPATH="$DITA_DIR/lib:$DITA_DIR/lib/dost.jar:$DITA_DIR/lib/resolver.jar:$DITA_DIR/lib/fop.jar:$DITA_DIR/lib/avalon-framework-cvs-20020806.jar:$DITA_DIR/lib/batik.jar:$DITA_DIR/lib/xalan.jar:$DITA_DIR/lib/xercersImpl.jar:$DITA_DIR/lib/xml-apis.jar:$DITA_DIR/lib/icu4j.jar"
+if test -n "$CLASSPATH"
+then
+export CLASSPATH="$NEW_CLASSPATH":"$CLASSPATH"
+else
+export CLASSPATH="$NEW_CLASSPATH"
+fi
+
+# Render the deliverable
+
+ant -Ddita.dir=$DITA_DIR -Dproject.dir=$PROJ_DIR -f $PROJ_DIR/build/agavireference_xhtml.xml -logger org.dita.dost.log.DITAOTBuildLogger $1 
+
+exit 1
+# Copy javascript
+cp $PROJ_DIR/shjs/* $PROJ_DIR/html-out/topics
+
+# Copy images
+
+echo "Copying images"
+
+mkdir -p $PROJ_DIR/html-out/topics/images
+cp $PROJ_DIR/svg/*.png $PROJ_DIR/html-out/topics/images
+# Create the stage tarballs
+
+mkdir $PROJ_DIR/html-out/stages
+
+cd $PROJ_DIR/stages
+
+for i in *
+do
+    tar czf $PROJ_DIR/html-out/stages/$i.tgz $i
+done
+
+cd $PROJ_DIR
+
+rm -rf agavi-tutorial.tgz
+tar czf agavi-tutorial.tgz html-out/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
