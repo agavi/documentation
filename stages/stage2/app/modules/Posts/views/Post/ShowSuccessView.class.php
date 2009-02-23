bd487e29f4ab9db9d8cd506078c9965248568e3f
Felix Gilcher <felix.gilcher@asquera.de>
2009-02-23 11:42:44 +0000

    - moved chapter for the static index page to creating-new-module - added chapter about creating actions - added a new action to the stage2 app

---
+++ stages/stage2/app/modules/Posts/views/Post/ShowSuccessView.class.php
@@ -0,0 +1,25 @@
+<?php
+
+class Posts_Post_ShowSuccessView extends BlogPostsBaseView
+{
+	public function executeHtml(AgaviRequestDataHolder $rd)
+	{
+		$this->setupHtml($rd);
+		
+		$post = array(
+			'id' => 1,
+			'title' => 'First post',
+			'posted' => '2008-07-14 00:01:07',
+			'category_name' => 'No category',
+			'author_name' => 'Admin',
+			'url' => null,
+			'content' => '<p>Terrific! This is our first post!</p>\r\n\r\n<p>This is just a first post. It has no actual contents. If you are reading this, things must be working.</p>'
+		);
+
+		$this->setAttribute('post', $post);
+		
+		$this->setAttribute('_title', $post['title']);
+	}
+}
+
+?>
