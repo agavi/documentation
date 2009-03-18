f39fc590e855d7d1e1e830116c850cd211bc7fad
Felix Gilcher <felix.gilcher@asquera.de>
2009-03-18 08:38:20 +0000

    once again rebuilding the stage apps

--- stages/stage2/app/modules/Posts/templates/Post/ShowSuccess.php
+++
@@ -1,8 +0,0 @@
-<?php
-// alias the post, to make access shorter
-$post = $t['post'];
-?>
-<span class="author">by <?php echo htmlspecialchars($post['author_name']); ?></span>
-<span class="category">in <?php echo htmlspecialchars($post['category_name']); ?></span>
-<span class="posted"><?php htmlspecialchars($post['posted']); ?></span>
-<div class="content"><?php echo $post['content']; ?></div>
--- stages/stage2/app/templates/exceptions/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-header('HTTP/1.0 500 Internal Server Error');
-header('Content-Type: text/plain');
-
-?>
-Error.
