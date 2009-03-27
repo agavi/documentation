3ee20fb70e8227c550b5806bd4f38d6e2de0bd01
Felix Gilcher <felix.gilcher@asquera.de>
2009-03-27 10:26:50 +0000

    split the html-build script into two parts - one builds the html version, the second one does the post-processing for the website

--- build.sh
+++ build.sh
@@ -23,30 +23,6 @@ fi
 
 ant -Ddita.dir=$DITA_DIR -Dproject.dir=$PROJ_DIR -f $PROJ_DIR/build/agavitutorial_xhtml.xml -logger org.dita.dost.log.DITAOTBuildLogger $1 -debug
 
-# Go over the html files and cut off the doctype crap, dita XSLT processor ignores standalone attributes
-
-cd $PROJ_DIR
-for i in `find html-out/topics -name '*.html'` ; do echo Postprocessing $i ; sed -i '.bak' -e '1,/^$/ d' -e '/<br id="endmain"/,$ d'  -e's|src="\.\./images|src="images/tutorial|g' $i; echo '</div>' >> $i ; done
-
-# Flatten TOC links & cut out HTML structure
-
-lines=`wc -l html-out/index.html | awk '{print $1}' `
-start=`expr $lines`
-
-echo '<ol class="thirdlevel left">' > html-out/toc.html
-sed -e '1,7 d' \
-    -e "$start,$ d" \
-    -e 's|href="topics/tutorial|href="documentation/tutorial|g' \
-    -e 's|href="topics/concepts|href="documentation/tutorial|g' \
-    -e 's|href="topics/reference|href="documentation/tutorial|g' \
-    -e 's|<ul>|<ol>|g' \
-    -e 's|</ul>|</ol>|g' \
-    html-out/index.html  >> html-out/toc.html
-    
-# Correct the image links
-find html-out -name '*.html' -exec sed -i '.bak' -e 's|src="\.\./images|src="images/tutorial|g' {} \;
-find html-out -name '*.bak' -delete
-
 # Copy javascript
 
 cp $PROJ_DIR/shjs/* $PROJ_DIR/html-out/topics
