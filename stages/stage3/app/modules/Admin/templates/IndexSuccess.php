a8e4c8ed937c9fd473554fc0f3e7125ba124a0e4
Felix Gilcher <felix.gilcher@asquera.de>
2009-07-06 20:56:36 +0000

    removing staging apps for backmerge, somehow svn mucks up if I don't

--- stages/stage3/app/modules/Admin/templates/IndexSuccess.php
+++
@@ -1,5 +0,0 @@
-<h1>Bloggie Administration</h1>
-<p>Welcome to Bloggie!</p>
-
-<h3><a href="<?php print $ro->gen('admin.posts.list'); ?>">Post management &raquo;</a></h3>
-<h3><a href="<?php print $ro->gen('admin.posts.create'); ?>">Create new post &raquo;</a></h3>
--- stages/stage3/app/modules/Public/templates/ShowNavigationSuccess.php
+++
@@ -1,3 +0,0 @@
-<?php foreach ($template['categories'] as $category): ?>
-<a href="<?php print $ro->gen('show_category', array('category_id' => $category['id'])); ?>"><?php print $category['name']; ?></a>
-<?php endforeach; ?>
